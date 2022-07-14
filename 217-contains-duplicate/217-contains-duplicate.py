59f6ed158877b97b611f866a7b2995cd1be5c26f
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-07-14 01:13:20 -0400

    Time: 702 ms (42.05%), Space: 26.1 MB (5.09%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -13,16 +13,3 @@ class Solution:
                 visitedValues.add(n)
             # return false if no duplicates in the set
         return False
-        
-        
-        # Use hashset to place visited values
-        # if value is in hashmap, then return true
-        # else return false
-        
-        
-        
-        
-        
-        # passing nums[int]
-        # return true is any value appears at least twice
-        # return false if every element is distinct
