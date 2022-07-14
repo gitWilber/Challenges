125acd1df3e6efcf52c7394bfb92bd291d84fc48
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-07-14 00:45:52 -0400

    Time: 1137 ms (87.60%), Space: 25 MB (85.25%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
+        # left and right pointer for difference
         left, right = 0, 1
+        # max profit variable
         maxProfit = 0
         
+        # while right is in bounds (length of the prices list)
         while right < len(prices):
+            # temp profit is difference between two pointers
             tempProfit = prices[right] - prices[left]
-            
+            # if there is a profit, we compare to temp and max profit
             if tempProfit >= 0:
                   maxProfit = max(maxProfit, tempProfit)
+            # else left pointer will equal to the right one
             else: 
                 left = right
+            # always increment right pointer
             right += 1
-        
+        # return max profit
         return maxProfit
