b9ddd85a5d1bbde8b8ba8f58eb59b37b44aa483b
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-07-14 01:12:54 -0400

    Time: 523 ms (77.04%), Space: 26 MB (72.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,13 +1,17 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
+        # Use a set to place visited numbers
         visitedValues = set()
-        
+        # for number in the list
         for n in nums:
+            # if the number is in the set, there are duplicates
             if n in visitedValues:
                 return True
+            # else we add the number into the set
             else:
                 visitedValues.add(n)
+            # return false if no duplicates in the set
         return False
         
         
