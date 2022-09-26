89d400cd3d9e690a512d58c8a976c12f7813e7b9
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-09-26 18:32:30 -0400

    Time: 68 ms (35.53%), Space: 15.3 MB (93.56%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,21 +5,18 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
         
+        prev = None
+        curr = head
         
-        # Use two pointers, a previous and current pointer
-        # current pointer will point to previous
-        
-        # Loop, while curr is not null, we can switch pointers and iterate
-        while curr:
-            # Use a temp node to point current value to right, so we can iterate once we switch pointers
-            tempNext = curr.next
-            # point node to previous node
+        while(curr):
+            
+            # temp hold next value
+            temp = curr.next
+            # current pointer will point to prev
             curr.next = prev
-            # shift previous node to equal to current node
             prev = curr
-            # shift current node to temp next node which is to the right
-            curr = tempNext
+            curr = temp
         
+        # return reverse LL
         return prev
