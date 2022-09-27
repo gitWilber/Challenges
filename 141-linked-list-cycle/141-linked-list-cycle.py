6c85fe3f4cc06ee2db005aab1eeffcb67bfc2a64
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-09-27 00:20:48 -0400

    Time: 124 ms (16.47%), Space: 17.5 MB (66.82%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,23 +6,18 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow, fast = head, head
         
-        # While fast pointer is not null
+        fast, slow = head, head
+        
+        # while fast node and fast pointer aren't null
         while fast and fast.next:
-            # iterate slow pointer by 1
-            slow = slow.next
-            # iterate fast pointer by 2
+            # shift fast pointer by 2
             fast = fast.next.next
-            # if both pointers meet/ equal to the same node, return true
+            # shift slow pointer by 1
+            slow = slow.next
+            # if fast pointer catches up to slow pointer, there's a cycle in the LL
             if fast == slow:
                 return True
-        # if the fast pointer does equal to null that means there is no loop.
-        # the list stops
+        # if fast node and its pointer ever equal null, that means there's a dead end
+        # there's no cycle
         return False
-    
-        # To determine a cycle in a linked list, we can use two pointers
-        # One pointer is at the head, and the other is ahead by two
-        # If both pointers ever equal to each other, that must mean there is a cycle
-        # Becaue the second pointer cycled and went back
-        # Floyd's Tortoise and Hare Algorithm
