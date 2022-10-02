d90ae1cdbd19c6bcd04d16b01f82bd189095408a
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-10-02 02:55:01 -0400

    Time: 2367 ms (28.68%), Space: 25 MB (38.03%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,22 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        # left and right pointer for difference
-        left, right = 0, 1
-        # max profit variable
+        # can use 2 pointers to subtract
+        buy, sell = 0, 1
         maxProfit = 0
         
-        # while right is in bounds (length of the prices list)
-        while right < len(prices):
-            # temp profit is difference between two pointers
-            tempProfit = prices[right] - prices[left]
-            # if there is a profit, we compare to temp and max profit
-            if tempProfit >= 0:
-                  maxProfit = max(maxProfit, tempProfit)
-            # else left pointer will equal to the right one
-            else: 
-                left = right
-            # always increment right pointer
-            right += 1
-        # return max profit
+        # while second pointer in range of list
+        for sell in range(len(prices)):
+            # temp profit is price of day sold - day bought
+            tempProfit = prices[sell] - prices[buy]
+            # if temp profit is not negative
+            if tempProfit > 0:
+                # compare profits to see which is max
+                maxProfit = max(maxProfit, tempProfit)
+            # else we move the first pointer
+            else:
+                buy = sell
+            # increment second pointer
+            sell += 1
+        # return max profit after comparing all days
         return maxProfit
+            
+        
+        
+        
+        
+        # passing array prices
+        # return the maxProfit
+        # i represent day
