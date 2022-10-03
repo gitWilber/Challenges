e9de8571a4bf8acbe2d930723a9d82106537f62d
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-10-03 01:19:07 -0400

    Time: 53 ms (90.49%), Space: 14.5 MB (67.00%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,22 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Use hashmaps to count the amount of times each character shows up in each string
-        sCharCount = {}
-        tCharCount = {}
-        
-        # if length of both strings aren't the same, they can't be anagrams
+       
+        # if both string aren't same length, can't be anagrams
         if len(s) != len(t):
             return False
-        # count chars in each string and put it in their hashmap
-        else:
-            for i in range(len(s)):
-                # use .get() incase there is no key. without it, it will be an error
-                sCharCount[s[i]] = 1 + sCharCount.get(s[i], 0)
-                tCharCount[t[i]] = 1 + tCharCount.get(t[i], 0)
-            
-        # compare both hashmaps to see if the same
-        if sCharCount != tCharCount:
+        
+        # use 2 hashmaps to put the number of times each letter appears in each string
+        hashmapS = {}
+        hashmapT = {}
+        
+        # loop to fill up both hashmaps
+        for i in range(len(s)):
+            #  hashmap key will equal to count of current occurence
+            hashmapS[s[i]] = 1 + hashmapS.get(s[i], 0)
+            hashmapT[t[i]] = 1 + hashmapT.get(t[i], 0)
+        
+        # if both hashmaps are not the same
+        if hashmapT != hashmapS:
             return False
-        # return true if it passes both tests
+        
+        # if both strings are same length
+        # if both occurence hashmaps are the same
+        # then its an anagram
         return True
-            
