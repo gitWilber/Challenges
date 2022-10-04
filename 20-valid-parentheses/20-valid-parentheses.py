46adf9e0937c2a161b4d31617b6bd96ebf8bed68
gitWilber <47231011+gitWilber@users.noreply.github.com>
2022-10-04 00:18:31 -0400

    Time: 70 ms (11.17%), Space: 13.8 MB (72.26%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,23 +1,28 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # hashMap to show matching parentheses
-        matchBrackets = {')' : '(', '}' : '{', ']' : '['}
+    
+        
+        # hashmap for matching brackets
+        validBrackets = {')' : '(', '}' : '{', ']' : '['}
+        # stack for pushing and popping elements
         stack = []
         
-        # string is an array we can loop
-        for char in s:
-            # if closing bracket in hashmap
-            if char in matchBrackets:
-                # if stack is not empty and its last element is the matching bracket to char
-                if stack and stack[-1] == matchBrackets[char]:
-                    # pop stack
+        # iterate through element in string
+        for i in s:
+            # if element is a closing bracket
+            if i in validBrackets:
+                # if stack is not empty and top element matches i in hashmap
+                if stack and stack[-1] == validBrackets[i]:
+                    # pop element in stack
                     stack.pop()
-                # else false because we can't add a closing bracket in the start
+                # else stack is empty and first element is closing
                 else:
                     return False
+            # push into stack if opening bracket
             else:
-                stack.append(char)
-                
-        # return true if stack is not empty, else return false
-        return True if not stack else False
-                
+                stack.append(i)
+        # if the stack is empty
+        if not stack:
+            return True
+                    
+            
